d00b9099df807bb95e31c34a4021158ac59b21fb
Alasdair Winter <aw@hep.ph.bham.ac.uk>
2014-11-03 14:48:43 +0000

    Adding original ex2

---
+++ ex2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+double a;
+double b;
+double c;
+ std::cout<< "Enter two numbers\n";
+std::cin>>a>>b;
+ c =a*b;
+ std::cout<< "The product of these is:  "<<c <<std::endl;
+
+return 0;
+}
